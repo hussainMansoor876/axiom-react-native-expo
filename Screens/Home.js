b01f63a373978159959ce04f1c9d844c91c8f981
mansoor876 <mansoor.hussain@mach3bi.com>
2021-12-04 15:44:08 +0500

    class 4

--- Screens/Home.js
+++ Screens/Home.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import { View, Text } from 'react-native'
+import { WebView } from 'react-native-webview'
 
 const Home = (props) => {
     return (
-        <View>
-            <Text>Home Page</Text>
+        <View style={{ flex: 1 }}>
+            <WebView
+                source={{ uri: 'http://hussainmansoor.com' }}
+            />
         </View>
     )
 }
